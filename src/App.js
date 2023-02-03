850cec22b595eeee3c711d6d0d8539963291a795
ABHISHEK KUMAR THAKUR <121752846+abhishekth0203@users.noreply.github.com>
2023-02-03 21:09:12 +0530

    using ErrorBoundry

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component  } from "react";
-
+import ErrorBoundary from "./Component/ErrorBoundary";
 import Hero from "./Component/Hero";
 import Main from "./Component/Main";
 
@@ -7,9 +7,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
-     
+      <ErrorBoundary>
+      <Hero name="joker" />
+      </ErrorBoundary>
+
+
+      <ErrorBoundary>
       <Main/>
-     
+      </ErrorBoundary>
         
       </div>
     );
--- src/Component/ErrorBoundary.js
+++ src/Component/ErrorBoundary.js
@@ -1,26 +1,26 @@
-// import React, { Component } from 'react'
+import React, { Component } from 'react'
 
-// export default class ErrorBoundary extends Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             isError: false,
-//         };
-//     }
+export default class ErrorBoundary extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isError: false,
+        };
+    }
 
-//     static getDerivedStateFromError(error) {
-//         return {isError: true };
-//     }
+    static getDerivedStateFromError(error) {
+        return {isError: true };
+    }
 
-//     componentDidCatch(err, errinfo){
-//         console.log(err);
-//         console.log(errinfo);
-//     }
-//   render() {
-//     if (this.state.isError) {
+    componentDidCatch(err, errinfo){
+        console.log(err);
+        console.log(errinfo);
+    }
+  render() {
+    if (this.state.isError) {
         
-//         return  <div className='hero'>you have error</div>;
-//     }
-//     else return <div>{this.props.children}</div>;
-//   }
-// }
+        return  <div className='hero'>you have error</div>;
+    }
+    else return <div>{this.props.children}</div>;
+  }
+}
