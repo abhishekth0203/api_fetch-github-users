e5ccf59e9e175b11cfe7d922d1dca824e25c7235
ABHISHEK KUMAR THAKUR <121752846+abhishekth0203@users.noreply.github.com>
2023-02-03 21:00:17 +0530

    fetch github users data

---
+++ src/Component/Hero.js
@@ -0,0 +1,16 @@
+// import React, { Component } from 'react'
+
+// export default class Hero extends Component {
+//     constructor(props){
+//         super(props);
+//         console.log("hero");
+//         console.log(props);
+//         if (props.name == "joker") {
+//             throw new Error("i  am not joker you are chombu");
+//         }
+//     }
+//   render() {
+//      return <div className='hero'>i am super hero</div>;
+    
+//   }
+// }
